525b4dfb7c6e2a66046ce3dc97c5a0b9b95afcbe
tranhuyhoangbka <hoangth92.nd@gmail.com>
2015-10-13 11:40:29 +0700

    play with wicked_pdf gem

---
+++ config/initializers/wicked_pdf.rb
@@ -0,0 +1,6 @@
+WickedPdf.config = {
+  # using "which wkhtmltopdf" command to see where is wkhtmltopdf
+  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
+  #:layout => "pdf.html",
+  :exe_path => '/home/likewise-open/FRAMGIA/tran.huy.hoang/.rvm/gems/ruby-2.1.0/bin/wkhtmltopdf'
+}
